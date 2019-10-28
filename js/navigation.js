b20225ba0c47d8f15fe184a2d6da0fcd7d539412
p77-web <paulfarcas13@gmail.com>
2019-10-28 15:57:35 -0400

    restart

--- js/navigation.js
+++ js/navigation.js
@@ -1,30 +1,10 @@
-sayswho = function() {
-	var ua = navigator.userAgent;
-	var M = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
-
-	// alert(navigator.platform);
-	return M[1];
-};
-
-console.log(navigator.platform);
 const hamburger = document.querySelector('#hamburger');
 const subtitle = document.querySelector('.header-content__subtitle');
 
 // const menu = document.getElementById('circle-icon');
 var isClicked = true;
 
-$(document).on('click touchstart', hamburger, function(event) {
-	event.preventDefault();
-	alert('sdfhjk');
-	if (window.navigator.userAgent.indexOf('iPhone') !== -1) {
-		subtitle.setAttribute('style', 'color: blue;');
-	} else {
-		subtitle.setAttribute('style', 'color: gray;');
-	}
-});
-
 window.addEventListener('load', () => {
-	sayswho();
 	hamburger.firstElementChild.setAttribute('style', 'animation: menu-animation-blue 1s ease-in forwards;');
 });
 
@@ -32,8 +12,6 @@ hamburger.addEventListener('click', e => {
 	const overlay = document.querySelector('.overlay');
 	const nav = document.querySelector('.navigation');
 	// const icon = document.querySelector('.menu-toggle span');
-
-	// alert('click');
 
 	overlay.classList.toggle('menu-open');
 	nav.classList.toggle('menu-open');
--- js/sign.js
+++ js/sign.js
@@ -7,15 +7,8 @@ const changeSign = e => {
 	const signAbout = document.getElementById('sign-about');
 	const signLanguages = document.getElementById('sign-languages');
 	const signPortfolio = document.getElementById('sign-portfolio');
-	const subtitle = document.querySelector('.header-content__subtitle');
 
-	console.log(subtitle);
 	if (signAbout.getBoundingClientRect().top < 400) {
-		if (window.navigator.userAgent.indexOf('iPhone') !== -1) {
-			subtitle.setAttribute('style', 'color: green;');
-		} else {
-			subtitle.setAttribute('style', 'color: blue;');
-		}
 		signAbout.firstElementChild.setAttribute('style', 'animation: sign-about-animation 1s ease-in forwards;');
 	}
 
