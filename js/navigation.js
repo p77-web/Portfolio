a6f5dbdc6511acd5cfb00b7d8d0b7610ce530b19
p77-web <paulfarcas13@gmail.com>
2019-10-28 18:57:10 -0400

    nav test

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('#hamburger');
-const subtitle = document.querySelector('.header-content__subtitle');
+const menu_input = document.querySelector('.menu-container input');
 
 // const menu = document.getElementById('circle-icon');
 var isClicked = true;
@@ -8,13 +8,20 @@ window.addEventListener('load', () => {
 	hamburger.firstElementChild.setAttribute('style', 'animation: menu-animation-blue 1s ease-in forwards;');
 });
 
-hamburger.addEventListener('click', e => {
+menu_input.addEventListener('click', e => {
+	// console.log(menu_input.checked);
+
 	const overlay = document.querySelector('.overlay');
 	const nav = document.querySelector('.navigation');
 	// const icon = document.querySelector('.menu-toggle span');
 
-	overlay.classList.toggle('menu-open');
-	nav.classList.toggle('menu-open');
+	if (menu_input.checked) {
+		overlay.classList.toggle('menu-open');
+		nav.classList.toggle('menu-open');
+	} else {
+		overlay.classList.toggle('menu-open');
+		nav.classList.toggle('menu-open');
+	}
 
 	if (isClicked) {
 		// icon.classList.remove('fa-bars');
@@ -30,4 +37,26 @@ hamburger.addEventListener('click', e => {
 		isClicked = true;
 	}
 	e.preventDefault();
+});
+
+hamburger.addEventListener('click', e => {
+	// const overlay = document.querySelector('.overlay');
+	// const nav = document.querySelector('.navigation');
+	// // const icon = document.querySelector('.menu-toggle span');
+	// if (menu_input.checked) {
+	// 	overlay.classList.toggle('menu-open');
+	// 	nav.classList.toggle('menu-open');
+	// }
+	// if (isClicked) {
+	// 	// icon.classList.remove('fa-bars');
+	// 	// icon.classList.add('fa-times');
+	// 	hamburger.firstElementChild.setAttribute('style', 'animation: menu-animation-red .5s ease-in forwards;');
+	// 	isClicked = false;
+	// } else {
+	// 	// icon.classList.remove('fa-times');
+	// 	// icon.classList.add('fa-bars');
+	// 	hamburger.firstElementChild.setAttribute('style', 'animation: menu-animation-blue 1s ease-in forwards;');
+	// 	isClicked = true;
+	// }
+	// e.preventDefault();
 });
