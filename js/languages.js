79df2a4d6eddc80591a4d5a61b6713aead5389f6
p77-web <paulfarcas13@gmail.com>
2019-11-01 19:28:49 -0400

    loading page counter added

--- js/languages.js
+++ js/languages.js
@@ -1,42 +1,5 @@
-// window.addEventListener(
-// 	'DOMContentLoaded',
-// 	function() {
-// 		let stops = document.getElementById('bar').getElementsByTagName('stop');
-// 		let position = document.getElementById('languages');
-
-// 		const stopArr = [...stops];
-// 		stopArr.shift();
-// 		stopArr.pop();
-
-// 		var html = document.documentElement;
-
-// 		const changeBarValue = function(e) {
-// 			for (const stop of stopArr) {
-// 				if (stopArr.length) {
-// 					// if (html.scrollHeight - html.clientHeight === 0) {
-// 					// 	stop.setAttribute('offset', 100 + '%');
-// 					// } else {
-// 					// }
-// 					const offset = stop.getAttribute('offset');
-
-// 					if ((window.scrollY / (html.scrollHeight - html.clientHeight)) * 100 <= 30) {
-// 						stop.setAttribute('offset', (window.scrollY / (html.scrollHeight - html.clientHeight)) * 30 + '%');
-// 					}
-// 				}
-// 			}
-// 		};
-// 		if (position.getBoundingClientRect().y > 500) {
-// 			document.addEventListener('scroll', changeBarValue, true);
-// 			console.log('bvsdjhfdks');
-// 		}
-// 	},
-// 	false
-// );
-
 window.addEventListener('scroll', function(e) {
 	changePosition();
-	// changeValue();
-	// changeTitleValue();
 	changeBarValue();
 });
 
@@ -62,31 +25,6 @@ let limit = 0;
 let titleLimit = 0;
 let barLimit = 0;
 
-const changeValue = function(e) {
-	let percentages = document.querySelectorAll('.languages__percentage');
-	let position = document.getElementById('languages');
-
-	if (position.getBoundingClientRect().top < 300 && limit === 0) {
-		percentages.forEach(function(percentage) {
-			let counter = 0;
-
-			setInterval(function() {
-				if (counter < parseInt(percentage.dataset.percentage, 10)) {
-					counter += 1;
-					percentage.innerHTML = counter + '%';
-				}
-			}, 20);
-		});
-		limit = 1;
-	}
-	if (position.getBoundingClientRect().top > 1000) {
-		percentages.forEach(function(percentage) {
-			percentage.innerHTML = 0 + '%';
-			limit = 0;
-		});
-	}
-};
-
 const changeBarValue = function(e) {
 	let percentages = document.querySelectorAll('.languages__bar');
 	let position = document.getElementById('languages');
@@ -111,32 +49,3 @@ const changeBarValue = function(e) {
 		});
 	}
 };
-
-// const changeTitleValue = e => {
-// 	let stops = document.getElementById('js').getElementsByTagName('stop');
-// 	let position = document.getElementById('languages');
-// 	let counter = 0;
-
-// 	const stopArr = [...stops];
-// 	stopArr.shift();
-// 	stopArr.pop();
-
-// 	var html = document.documentElement;
-// 	for (const stop of stopArr) {
-// 		if (stopArr.length) {
-// 			if (position.getBoundingClientRect().y < 300 && titleLimit === 0) {
-// 				setInterval(function() {
-// 					if (counter < 100) {
-// 						counter += 1;
-// 						stop.setAttribute('offset', (window.pageYOffset / (html.scrollHeight - html.clientHeight)) * counter + '%');
-// 					}
-// 				}, 20);
-// 				titleLimit = 1;
-// 			}
-// 			if (position.getBoundingClientRect().y > 1000) {
-// 				stop.setAttribute('offset', 0 + '%');
-// 				titleLimit = 0;
-// 			}
-// 		}
-// 	}
-// };
---
+++ js/load-counter.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function(e) {
+	changeLoadValue(performance.now());
+});
+
+const changeLoadValue = function(time) {
+	let percentages = document.querySelector('.counter__percentage');
+	let overlay = document.querySelector('.load-overlay');
+	let counter_wrapper = document.querySelector('.counter');
+	let circle = document.getElementById('overlay-circle');
+	let counter = 0;
+
+	setInterval(function() {
+		if (counter < parseInt(percentages.dataset.percentage, 10)) {
+			counter += 1;
+			percentages.innerHTML = counter + '%';
+			// circle.setAttribute('style', 'animation: circle-animation ' + time / 100 + 's ease-in;');
+		}
+		circle.setAttribute('style', 'animation: circle-animation ' + time / 1000 + 's ease-in;');
+	}, time / 100);
+
+	setInterval(function() {
+		overlay.setAttribute('style', 'background-color: rgba(0, 0, 0, 0); transition: background-color 1s ease-out;');
+		counter_wrapper.setAttribute('style', 'display: none;');
+	}, time + 500);
+	circle.setAttribute('style', 'display: none;');
+};
